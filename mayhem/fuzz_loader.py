b11e25418f0215fe8d5c0463262687b9b36faa58
bcapuano <bcapuano@asu.edu>
2022-11-15 20:38:57 -0500

    Initial Mayhem Heroes Integration

---
+++ mayhem/fuzz_loader.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import atheris
+import io
+import sys
+
+with atheris.instrument_imports():
+    import speakeasy
+    import speakeasy.errors
+
+@atheris.instrument_func
+def TestOneInput(data):
+    try:
+        se = speakeasy.Speakeasy()
+        module = se.load_module(data=data)
+        se.run_module(module)
+        se.get_report()
+    except speakeasy.errors.SpeakeasyError:
+        return -1
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == '__main__':
+    main()
